b3a0e20519e0f52ab55b222e0d6bcff9e9d0d662
mahdi <mahdideveloper80@gmail.com>
2024-11-21 14:11:59 +0330

    Ladning Astro

---
+++ script.js
@@ -0,0 +1,8 @@
+function darkModeToggle() {
+  document.documentElement.classList.toggle("dark");
+}
+
+function toggleMenu() {
+  document.body.classList.toggle("open_menu");
+  document.getElementById("menu").classList.toggle("hidden");
+}
